e7e94df182b3c571093f08cf50bb758c3e67b5b6
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-12 00:31:02 +0530

    start

--- functions/start.cpp
+++ functions/start.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int sum(int t,int s) {
      int add = t + s;
     return add;
+    // return keyword is used to end with the datatype values user provided
 
 }
+// main function always starts at beginning
 int main() {
     // sum(4,5);
     cout<<sum(4,5);
