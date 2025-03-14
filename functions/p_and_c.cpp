6ed2f8e6b87e3861bc20bd0821061d59a7018498
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-14 18:08:10 +0530

    func

---
+++ functions/p_and_c.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int fact(int x){
+    int f =1;
+    for(int i=2; i <= x; i++){
+        f *= i;
+    }
+}
+int combination( int n, int r){
+    int ncr = fact(n)/fact(r)* fact(n-r);
+}
+int permutation( int n, int r){
+    int ncr = fact(n)/fact(n-r);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int r;
+    cin>>r;
+    int ncr = combination(n,r);
+    int npr = permutation(n,r);
+    cout<<ncr<<endl<<npr;
+}
