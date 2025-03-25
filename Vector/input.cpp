5ebdb627edfdc45ebd1d4eae2e98fd8b6f144234
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-25 05:47:50 +0530

    Vectors

---
+++ Vector/input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector <int> v(5); // declaring size of array
+    // Taking input
+    for(int i=0; i< 5; i++) {
+        cin>>v[i];
+    }
+    // Showing output
+    for(int i=0; i< 5; i++) {
+        cout<<v[i]<<" ";
+    }
+
+
+}
