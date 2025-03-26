fabfd1262ec2214fe533121dc9b72fb48e959e85
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-26 05:50:12 +0530

    vectors

--- Vector/looping.cpp
+++ Vector/looping.cpp
@@ -8,13 +8,19 @@ int main() {
     v.push_back(6);
     v.push_back(8);
     v.push_back(2);
-    v.push_back(8);
+    v.push_back(6);
     v.push_back(5);
 
-    int x = 2;
+    int x = 6;
     int idx = -1; // element whose last index has to be determined
-    for(int i=0; i< v.size(); i++){
-        if(v[i] == x) idx = i;
+    // for(int i=0; i< v.size(); i++){
+    //     if(v[i] == x) idx = i;
+    // }
+    for(int i = v.size() -1; i>= 0; i--) {
+        if(v[i]== x) {
+            idx = i;
+            break;
+        }
     }
     cout<<idx;
 
