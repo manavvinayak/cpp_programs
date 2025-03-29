fd7289ef247e98cbbe8bbe44cd30781cef120ab4
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-03-29 05:50:42 +0530

    Array3

--- Vector/sort01.cpp
+++ Vector/sort01.cpp
@@ -16,8 +16,24 @@ void sort01(vector <int> & v) {
         if(i < no0) v[i] =0;
         else v[i] = 1;
     }
-    
+}
 
+void sort01Diff (vector<int> &v) {
+    int n = v.size();
+    int i = 0;
+    int j = n -1;
+    while(i < j) {
+        if (v[i] == 0) i++;
+        if (v[j] == 1) j--;
+        if( i > j) break;
+        if(v[i] == 1 && v[j] == 0) {
+            // Swap
+            v[i] = 0;
+            v[j] = 1;
+            i++;
+            j--;
+        }
+    }
 }
 
 int main() {
@@ -33,7 +49,7 @@ int main() {
         cout<<v[i]<<" ";
     }
     cout<<endl;
-    sort01(v);
+    sort01Diff(v);
     for(int i=0;i<v.size(); i++) {
         cout<<v[i]<<" ";
     }
