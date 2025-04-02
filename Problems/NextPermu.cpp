64d252a2e0d02b8f5b008b7aebdffd56c0c6b0db
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-04-02 13:39:25 +0530

    Next permutation by Hippo

---
+++ Problems/NextPermu.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> v;
+    
+    int copy;
+    v.push_back(9);
+    v.push_back(8);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(5);
+    v.push_back(4);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(1);
+    int n = v.size();
+    for(int i=0; i< n; i++) {
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=n-1; i >= 1; i--) {
+        if(v[i] > v[i-1]) {
+            copy = v[i-1];
+            break;
+        }
+    }
+    cout<<copy;
+   
+}
