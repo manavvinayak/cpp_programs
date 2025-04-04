1721a8a5aa0bfd33f2100010b4f51f2cccab53eb
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-04-04 05:32:58 +0530

    2D Arrays

---
+++ 2dArray/declaration.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3] [3] = {{1,2,3}, {5,7,8}, {9,5,6}};
+    // arr[no. of rows] [no. of columns]
+    // 2d arrays are array of arrays
+    cout<<arr[0] [0]<<"   ";
+    cout<<arr[1] [2];
+}
