3581de0c5e54bd503b9b40af94d50c45c6fa8165
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-04-13 17:05:01 +0530

    waves in 2d

---
+++ 2dArray/wave_print2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m;
+    cout<<"Enter rows of  matrix: ";
+    cin>>m;
+    int n;
+    cout<<"Enter col of  matrix: ";
+    cin>>n;
+    int arr[m] [n];
+    // print
+    for(int i=0; i < m; i++ ) {
+        for(int j=0; j < n; j++) {
+            cin>> arr[i] [j];
+        }
+        
+    }
+    cout<<endl;
+
+    // Wave 
+    for(int i=0; i < m; i++) {
+        if(i%2 != 0) {
+            for(int j=0; j < n; j++) {
+                cout<< arr[i] [j]<<" ";
+            }
+        }
+        else {
+            for(int j= n-1; j>= 0; j--) {
+                cout<<arr[i] [j]<< " ";
+            }
+        }
+        cout<<endl;
+    }
+}
