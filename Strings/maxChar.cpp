ee9bca61ebdde4fa48f67afcbd80d11499237476
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-06-08 08:51:15 +0530

    max

--- Strings/maxChar.cpp
+++ Strings/maxChar.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
+    // print character occuring most number of times
     string s;
     getline(cin, s);
-    int max = 0;
-    for(int i=0; i < s.size(); i++) {   
-        if(s[i] > max) max = s[i];
-    }
-    cout<<max;
+    string max;
+    for(int i = 0; i < s.size() -1 ; i++){
+        {
+            for( int j = i+1; j < s.size(); j++) {
+               if(s[i] == s[i+1] && s[i] == s[j]  ) {max = s[i];}
+            }
+        }
+       
+    }    
+    cout<< max;
 }
