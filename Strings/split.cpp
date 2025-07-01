6b2bf13544c48fbf2ef84ba0704e13a90072e9f0
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-07-01 06:23:04 +0530

    problem of splitting the inputted string

---
+++ Strings/split.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string temp;
+    while ( ss >> temp){
+        cout<<temp<<endl;
+    }
+
+    return 0;
+}
