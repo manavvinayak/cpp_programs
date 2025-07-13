7840ebcb82bc9fbdd9a5a1aaa95aa2e7a90f3e0e
Vinayak Narayan <vinayak_srivastava22@manavrachna.net>
2025-07-13 07:32:18 +0530

    call

---
+++ Recursion/FuncCall.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+void gun() {
+    cout<<"Heyoo this is TW";
+}
+void fun() {
+    cout<<"Heyooo this is CW"<<endl;
+    gun();
+}
+int main() {
+    fun();
+    return 0;
+}
